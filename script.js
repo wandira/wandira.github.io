30ec9f7c51fda15bcf3c3f568d5c4c523f59ebb6
wandira <56605375+wandira@users.noreply.github.com>
2021-04-28 17:35:48 +0700

    fix randomizer and logic

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const bg = document.getElementsByClassName("main")[0];
 const audio = Array.from(document.getElementsByClassName("audio"));
 
-const rand = Math.floor((Math.random() * 10) + 1);
-
 const playSound = (e) => {
   console.log("yey");
-  if (e.keyCode==70) {
+  if (e.keyCode == 70) {
     audio[0].currentTime = 0.1;
     audio[0].play();
     bg.classList.remove("fadein");
     bg.classList.add("opaque");
-  } else if (e.keycode>63&&e.keycode<90) {
+  } else if (e.keyCode > 64 && e.keyCode < 91) {
+    const rand = Math.floor(Math.random() * 10 + 1);
     audio[rand].currentTime = 0.1;
     audio[rand].play();
   }
