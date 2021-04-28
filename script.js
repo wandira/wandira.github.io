64ea5c481f80eed0fab94b1c3de89fec6e69e8f8
wandira <56605375+wandira@users.noreply.github.com>
2021-04-28 11:15:29 +0700

    make sfx randomizer

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const bg = document.getElementsByClassName("main")[0];
-const audio = document.getElementById("audio");
+const audio = Array.from(document.getElementsByClassName("audio"));
+
+const rand = Math.floor((Math.random() * 10) + 1);
 
 const playSound = (e) => {
   console.log("yey");
-  if (e.keyCode == 70) {
-    audio.currentTime = 0.1;
-    audio.play();
+  if (e.keyCode==70) {
+    audio[0].currentTime = 0.1;
+    audio[0].play();
     bg.classList.remove("fadein");
     bg.classList.add("opaque");
+  } else if (e.keycode>63&&e.keycode<90) {
+    audio[rand].currentTime = 0.1;
+    audio[rand].play();
   }
 };
 const removeOpaque = (e) => {
