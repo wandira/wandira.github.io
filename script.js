3be6d181a64556e86900ab593f335f6a7bbcfca2
WANDIRAST <wandira.milleona@styletheory.co>
2021-04-28 17:55:34 +0700

    use data-key in the script

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ const bg = document.getElementsByClassName("main")[0];
 const audio = Array.from(document.getElementsByClassName("audio"));
 
 const playSound = (e) => {
-  console.log("yey");
+  // console.log("yey");
   if (e.keyCode == 70) {
     audio[0].currentTime = 0.1;
     audio[0].play();
     bg.classList.remove("fadein");
     bg.classList.add("opaque");
-  } else if (e.keyCode > 64 && e.keyCode < 91) {
-    const rand = Math.floor(Math.random() * 10 + 1);
-    audio[rand].currentTime = 0.1;
-    audio[rand].play();
+  } else {
+    const a = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!a) {
+      const rand = Math.floor(Math.random() * 10 + 1);
+      audio[rand].currentTime = 0.1;
+      audio[rand].play();
+    } else {
+      a.currentTime = 0.1;
+      a.play();
+    }
   }
 };
 const removeOpaque = (e) => {
-  console.log(e);
+  // console.log(e);
   if (e.propertyName !== "opacity") return;
   bg.classList.remove("opaque");
   bg.classList.add("fadein");
